316b27998f72ad8d7672632df5e707d96108a0ce
GuoKaku <guo-jw21@mails.tsinghua.edu.cn>
2023-10-26 20:20:05 +0800

    refine img upload function with pyq-like widget

--- frontend/pages/show/event/event.js
+++ frontend/pages/show/event/event.js
@@ -1,4 +1,7 @@
 // pages/show/event/event.js
+
+
+
 Page({
 
   /**
@@ -11,7 +14,14 @@ Page({
     selectedTags: [], // 已选中的标签列表
     showInput: false, // 是否显示输入框
     inputValue: '', // 输入框的值
-    isEditing: false // 是否处于编辑模式
+    isEditing: false, // 是否处于编辑模式
+    imgGridStyle:`display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-content:center;
+    align-items:center;
+    width: 100%;
+    height:30vw;`
   },
   handleInput(e) {
     this.setData({
@@ -22,7 +32,10 @@ Page({
     console.log('提交的文本：', this.data.inputText);
     // 进行其他处理或操作
   },
-  handleUpload() {
+
+  chooseImage:function(e){
+    console.log("tapped")
+    const index = e.currentTarget.dataset.index; // 获取用户点击的格子索引
     wx.chooseMedia({
       count: 9 - this.data.imageList.length,
       mediaType: ['image'],
@@ -30,12 +43,32 @@ Page({
       success: (res) => {
         const tempFiles = res.tempFiles;
         const imageList = this.data.imageList.concat(tempFiles.map((file) => file.tempFilePath));
+        const num_rows=Math.ceil((imageList.length+1)/ 3);
+        console.log('num_rows',num_rows)
+        // var query = wx.createSelectorQuery().select('#image-grid-container').boundingClientRect(function (gridContainer) { 
+        //     gridContainer.gridTemplateRows = `repeat(${num_rows}, 1fr)`;
+        //     let grid_height=30*num_rows;
+        //     gridContainer.height=`${grid_height}vw`;
+        //   }).exec();
+
         this.setData({
-          imageList: imageList.slice(0, 9)
+          imageList: imageList.slice(0, 9),
+          imgGridStyle:`  display: grid;
+          grid-template-rows: repeat(${num_rows}, 1fr);
+          grid-template-columns: 1fr 1fr 1fr;
+          justify-content:center;
+          align-items:center;
+          width: 100%;
+          height:calc(30vw*${num_rows});`
         });
+        
+      },
+      fail: (res)=>{
+        console.log(1)
       }
     });
   },
+
   handleDelete(event) {
     const index = event.currentTarget.dataset.index;
     const imageList = this.data.imageList;
@@ -46,6 +79,7 @@ Page({
   },
   toggleTag: function(e) {
     const { index } = e.currentTarget.dataset;
+
     const { selectedTags } = this.data;
     const tag = this.data.tags[index].info;
     const tagIndex = selectedTags.indexOf(tag);
@@ -60,16 +94,16 @@ Page({
       selectedTags: selectedTags,
       ['tags[' + index + '].checked']: this.data.tags[index].checked 
     });
-    console.log(selectedTags);
+
     console.log("index",index)
     console.log(this.data.tags[index].checked)
-    
   },
   showInput: function() {
     this.setData({
       showInput: true
     });
   },
+
   addTag: function(e) {
     const { value } = e.detail;
     if (value.trim() !== '') {
