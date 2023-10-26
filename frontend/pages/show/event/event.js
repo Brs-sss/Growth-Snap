a9ab545908096362c8bf4ddf5756495943ea68ea
GuoKaku <guo-jw21@mails.tsinghua.edu.cn>
2023-10-26 23:59:48 +0800

    feat(front): event page is basically done with excellent shown style and outfit

--- frontend/pages/show/event/event.js
+++ frontend/pages/show/event/event.js
@@ -1,14 +1,6 @@
 // pages/show/event/event.js
 
-function getImageGridStyle(num_rows){
-  return `display: grid;
-  grid-template-rows: repeat(${num_rows}, 1fr);
-  grid-template-columns: 1fr 1fr 1fr;
-  justify-content:center;
-  align-items:center;
-  width: 100%;
-  height:calc(30vw*${num_rows});`
-}
+
 
 Page({
 
@@ -16,31 +8,28 @@ Page({
    * 页面的初始数据
    */
   data: {
-    inputTitle: '', //标题的名称
+    inputText: '',
     imageList: [],
     tags: [], // 已保存的标签列表
     selectedTags: [], // 已选中的标签列表
     showInput: false, // 是否显示输入框
-    inputText: '', // 输入框的值
-    inputTag:'',
+    inputValue: '', // 输入框的值
     isEditing: false, // 是否处于编辑模式
-    imgGridStyle:getImageGridStyle(1)
+    imgGridStyle:`display: grid;
+    grid-template-rows: 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    justify-content:center;
+    align-items:center;
+    width: 100%;
+    height:30vw;`
   },
-
-  handleInputTitle(e) {  //输入标题的处理
-    this.setData({
-      inputTitle: e.detail.value
-    });
-  },
-
-  handleInputText(e) {  //输入文字的处理
+  handleInput(e) {
     this.setData({
       inputText: e.detail.value
     });
   },
-
   handleSubmit() {
-    console.log('提交的文本：', this.data.inputTitle);
+    console.log('提交的文本：', this.data.inputText);
     // 进行其他处理或操作
   },
 
@@ -55,15 +44,24 @@ Page({
         const tempFiles = res.tempFiles;
         const imageList = this.data.imageList.concat(tempFiles.map((file) => file.tempFilePath));
         const num_rows=Math.ceil((imageList.length+1)/ 3);
-        this.setData({
-          imageList: imageList.slice(0, 9),
-          imgGridStyle:getImageGridStyle(num_rows)
-        });
+        console.log('num_rows',num_rows)
         // var query = wx.createSelectorQuery().select('#image-grid-container').boundingClientRect(function (gridContainer) { 
         //     gridContainer.gridTemplateRows = `repeat(${num_rows}, 1fr)`;
         //     let grid_height=30*num_rows;
         //     gridContainer.height=`${grid_height}vw`;
         //   }).exec();
+
+        this.setData({
+          imageList: imageList.slice(0, 9),
+          imgGridStyle:`  display: grid;
+          grid-template-rows: repeat(${num_rows}, 1fr);
+          grid-template-columns: 1fr 1fr 1fr;
+          justify-content:center;
+          align-items:center;
+          width: 100%;
+          height:calc(30vw*${num_rows});`
+        });
+        
       },
       fail: (res)=>{
         console.log(1)
@@ -71,39 +69,14 @@ Page({
     });
   },
 
-  changeImage:function(e){  //更改一张图片为其他图片
-    const index = e.currentTarget.dataset.index; // 获取用户点击的格子索引
-    wx.chooseMedia({
-      count: 1,
-      mediaType: ['image'],
-      sourceType: ['album', 'camera'],
-      success: (res) => {
-        const tempFiles = res.tempFiles;
-        const imageList = this.data.imageList;
-        imageList[index]=tempFiles[0].tempFilePath;
-        this.setData({
-          imageList: imageList,
-        });
-      },
-    });
-  },
-
   handleDelete(event) {
     const index = event.currentTarget.dataset.index;
     const imageList = this.data.imageList;
     imageList.splice(index, 1);
-    const num_rows=Math.ceil((imageList.length+1)/ 3);
-    console.log("rows",num_rows)
     this.setData({
-      imageList: imageList,
-      imgGridStyle:getImageGridStyle(num_rows)
+      imageList: imageList
     });
-    // 返回 false 阻止事件传播
-    return false
   },
-
-
-
   toggleTag: function(e) {
     const { index } = e.currentTarget.dataset;
 
@@ -145,20 +118,20 @@ Page({
         this.setData({
           tags: tags,
           showInput: false,
-          inputTag: ''
+          inputValue: ''
         });
       } else {
         tags.push({info: value, checked: false});
         this.setData({
           tags: tags,
           showInput: false,
-          inputTag: ''
+          inputValue: ''
         });
       }
     } else {
       this.setData({
         showInput: false,
-        inputTag: ''
+        inputValue: ''
       });
     }
   },
