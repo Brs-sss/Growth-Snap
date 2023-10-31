1889ea821c7ceefd1c0ce673a976e5dbced1f988
GuoKaku <guo-jw21@mails.tsinghua.edu.cn>
2023-10-31 19:25:59 +0800

    feat(back): realize upload multiple images and invent img-management protocol

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('api/login', views.login, name='login'),
     path('api/register', views.register, name='register'),
-    path('api/show/event/submit',views.submitEvent,name='submit_event')
+    path('api/show/event/submit',views.submitEvent,name='submit_event'),
+    path('api/getSHA256',views.getSHA256,name='getsha256'),
+    path('api/show/event/upload_image',views.addEventImage,name='upload_image'),
 ]
