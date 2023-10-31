ed51a9e165b1428ee88a0abfe92ecc42ba10046a
GuoKaku <guo-jw21@mails.tsinghua.edu.cn>
2023-11-01 00:04:30 +0800

    feat(back): events of history and new ones can show in mainpagegit reset ./backend/db.sqlite3

--- backend/user/urls.py
+++ backend/user/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('api/login', views.login, name='login'),
@@ -8,4 +12,7 @@ urlpatterns = [
     path('api/show/event/submit',views.submitEvent,name='submit_event'),
     path('api/getSHA256',views.getSHA256,name='getsha256'),
     path('api/show/event/upload_image',views.addEventImage,name='upload_image'),
+    path('api/show/all',views.loadShowPage,name='load_showpage'),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
