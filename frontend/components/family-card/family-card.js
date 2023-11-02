6a57fca403966f3cb779ebde663a37feaeecc47e
王皓雯 <3178243510@qq.com>
2023-11-02 20:18:26 +0800

    feat(both): show family members

---
+++ frontend/components/family-card/family-card.js
@@ -0,0 +1,25 @@
+Component({
+  properties: {
+    name: {
+      type: String,
+      value: '30',
+    },
+    label: {
+      type: String,
+      value: '5月',
+    },
+    signature: {
+      type: String,
+      value: '小明妈',
+    },
+    imgSrc: {
+      type: String,
+      value: '/image/temp/colorring.png',
+    },
+    
+    
+  },
+  methods: {
+    // 自定义方法
+  },
+});
