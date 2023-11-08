3f7b42a8dd906ec633a3806ceb0e0569056d5172
leishaforlinminzhi <15611664060@qq.com>
2023-11-08 18:45:27 +0800

    feat(front): implement add_event function

--- frontend/pages/generate/add_event/add_event.js
+++ frontend/pages/generate/add_event/add_event.js
@@ -14,40 +14,68 @@ Page({
   },
   toggleTag: function(e) {
     const { index } = e.currentTarget.dataset;
-
-    const { selectedTags } = this.data;
+    const { selectedTags, eventList } = this.data;
     const tag = this.data.tags[index].info;
     const tagIndex = selectedTags.indexOf(tag);
     if (tagIndex !== -1) {
+      eventList.forEach(event => {
+        if (selectedTags.some(selectedTag => event.tags.includes(selectedTag))) {
+          event.checked = false;
+        }
+      });
       selectedTags.splice(tagIndex, 1); // 取消选中
       this.data.tags[index].checked = false ;
     } else {
       selectedTags.push(tag); // 选中
       this.data.tags[index].checked = true ;
+      eventList.forEach(event => {
+        if (selectedTags.some(selectedTag => event.tags.includes(selectedTag))) {
+          event.checked = true;
+        }
+      });
     }
     this.setData({
       selectedTags: selectedTags,
+      eventList: eventList,
       ['tags[' + index + '].checked']: this.data.tags[index].checked 
     });
-
-    console.log("index",index)
-    console.log(this.data.tags[index].checked)
+  },
+  selectEvent: function(e) {
+    const { index } = e.currentTarget.dataset;
+    const { selectedEvents } = this.data;
+    const event = this.data.eventList[index].info;
+    const eventIndex = selectedEvents.indexOf(event);
+    if (eventIndex !== -1) {
+      selectedEvents.splice(eventIndex, 1); // 取消选中
+      this.data.eventList[index].checked = false ;
+    } else {
+      selectedEvents.push(event); // 选中
+      this.data.eventList[index].checked = true ;
+    }
+    this.setData({
+      selectedEvents: selectedEvents,
+      ['eventList[' + index + '].checked']: this.data.eventList[index].checked 
+    });
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    const category = decodeURIComponent(options.category);
-    const index = decodeURIComponent(options.index);
-    console.log('category', category);
+    // const category = decodeURIComponent(options.category);
+    // const index = decodeURIComponent(options.index);
     this.setData({
-      generateCategory: category,
-      templateIndex: index,
+      // generateCategory: category,
+      // templateIndex: index,
       tags: [
         {info: '亲子', checked: false},
         {info: '自然', checked: false},
         {info: '阅读', checked: false},
         {info: '生日', checked: false},
+      ],
+      eventList: [
+        {id: 0, title: '今天带小明感受大自然', tags: ['亲子', '自然'], checked: false},
+        {id: 1, title: '小明在书店买了自己喜欢的书', tags: ['亲子', '阅读'], checked: false},
+        {id: 2, title: '小明七岁生日', tags: ['生日'], checked: false},
       ]
     });
   },
