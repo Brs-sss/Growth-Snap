21cc678b12429caefe5495c17bbc277fdfaae4d2
GuoKaku <guo-jw21@mails.tsinghua.edu.cn>
2023-11-14 17:51:06 +0800

    feat(both): able to preview diary pdf

--- frontend/pages/generate/preview/preview.js
+++ frontend/pages/generate/preview/preview.js
@@ -1,9 +1,8 @@
 // pages/generate/preview/preview.js
 
-//const pdfjsLib = require('pdfjs-dist/build/pdf');
+//const pdfjsLib = require('../../../miniprogram_npm/miniprogram_npm/pdf.js');
+// const pdfjs = require('pdfjs-dist/webpack');
 const app = getApp();
-
-
 
 Page({
 
@@ -13,6 +12,8 @@ Page({
   data: {
     previewList:[],
     host_: `${app.globalData.localUrl}`,
+    pdf_url:null,
+    pdf_name:null,
   },
 
   renderPDF: function(url) {
@@ -45,6 +46,68 @@ Page({
     })
   },
 
+  WXpreviewPDF(pdf_url){
+    wx.showLoading({
+      title: '加载中...',
+    });
+    wx.downloadFile({
+      url: pdf_url, // 替换成你的后端 API 地址
+      success: function (res) {
+        if (res.statusCode === 200) {
+          const filePath = res.tempFilePath;
+          wx.openDocument({
+            filePath: filePath,
+            success: function (res) {
+              console.log('打开文档成功');
+            },
+            fail: function (res) {
+              console.log('打开文档失败', res);
+            },
+            complete: function () {
+              wx.hideLoading();
+            }
+          });
+        } else {
+          console.error('下载文件失败', res);
+          wx.hideLoading();
+        }
+      },
+      fail: function (res) {
+        console.error('下载文件失败', res);
+        wx.hideLoading();
+      }
+    });
+  },
+
+  AskForPreviewImages(that,openid,pdf_name,category){
+    wx.showLoading({
+      title: '加载中...',
+    });
+    wx.request({
+      url: that.data.host_+'user/api/generate/'+category+'/preview'+'?openid='+openid+'&file_name='+pdf_name,
+      method:'GET',
+      success:function(res){
+          that.setData({
+            previewList:res.data.imageList
+          })
+          console.log(res.data.imageList)
+      },
+      fail:function(res){
+        console.log('load page failed: ',res)
+      },
+      complete: function () {
+        wx.hideLoading();
+      }
+    })
+  },
+
+  pdfLoaded: function (e) {
+    console.log('PDF加载完成', e);
+  },
+  pdfError: function (e) {
+    console.error('PDF加载失败', e);
+  },
+
 
   /**
    * 生命周期函数--监听页面加载
@@ -58,39 +121,15 @@ Page({
       success: function (res) { 
         // 从本地存储中获取数据,在index.js文件中保存建立的
         let openid=res.data
-  
+        
         let pdf_url=that.data.host_+'static/diary/'+openid+'/'+pdf_name+'.pdf'
         //that.renderPDF(pdf_url);
-        wx.showLoading({
-          title: '加载中...',
-        });
-        wx.downloadFile({
-          url: pdf_url, // 替换成你的后端 API 地址
-          success: function (res) {
-            if (res.statusCode === 200) {
-              const filePath = res.tempFilePath;
-              wx.openDocument({
-                filePath: filePath,
-                success: function (res) {
-                  console.log('打开文档成功');
-                },
-                fail: function (res) {
-                  console.log('打开文档失败', res);
-                },
-                complete: function () {
-                  wx.hideLoading();
-                }
-              });
-            } else {
-              console.error('下载文件失败', res);
-              wx.hideLoading();
-            }
-          },
-          fail: function (res) {
-            console.error('下载文件失败', res);
-            wx.hideLoading();
-          }
-        });
+        //that.WXpreviewPDF(pdf_url)
+        that.AskForPreviewImages(that,openid,pdf_name,category)
+        that.setData({
+          pdf_url:pdf_url,
+          pdf_name:pdf_name,
+        })
 
       },
       fail: function (res) {
