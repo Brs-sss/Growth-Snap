27d869d7cc7ccd83ec99490414344c5d2289aa2e
Brs-sss <brs21@mails.tsinghua.edu.cn>
2023-11-14 11:54:37 +0800

    other

--- frontend/pages/plan/all_plan/all_plan.js
+++ frontend/pages/plan/all_plan/all_plan.js
@@ -5,33 +5,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-    planList: [],
-    host_: 'http://127.0.0.1:8090/'
+    planList: [{title:'代码能力提升', icon:'/image/plan/icons/computer.png'},
+               {title:'钢琴计划', icon:'/image/plan/icons/piano.png'},
+               {title:'英语考级', icon:'/image/plan/icons/piano.png'},
+               {title:'阴阳师', icon:'/image/plan/icons/piano.png'},
+               {title:'王者荣耀', icon:'/image/plan/icons/piano.png'},
+               {title:'原神', icon:'/image/plan/icons/piano.png'},],
   },
 
-  loadPage() {
-    var pointer = this
-    wx.getStorage({
-      key: 'openid',  // 要获取的数据的键名
-      success: function (res) { 
-        var openid = res.data
-        wx.request({
-          url: pointer.data.host_ + 'user/api/plan/all' + '?openid=' + openid,
-          method:'GET',
-          success:function(res){
-            pointer.setData({
-              planList: res.data.plan_list
-            })
-          },
-          fail:function (res) {
-            console.log('fail to get')
-            console.log(res)
-          }
-        });
-      },
-      fail: function(res) {
-        console.error('获取本地储存失败', res);
-      }
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    var bgList = []
+    var bgNum = 7
+    for(var i = 0; i < this.data.planList.length; i++){
+      bgList.push(i % bgNum)
+    }
+    console.log(bgList)
+    this.setData({
+      bgList: bgList
     })
   },
 
@@ -51,13 +44,6 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-    this.loadPage()
-  },
-
-  /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady() {
@@ -68,7 +54,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    this.loadPage()
+
   },
 
   /**
