f66100deb52f6426dba2a68e081b5aea580d593a
leishaforlinminzhi <15611664060@qq.com>
2023-11-17 21:39:28 +0800

    feat(front): implement basic template changing in chart page

--- frontend/pages/generate/chart/chart.js
+++ frontend/pages/generate/chart/chart.js
@@ -1,7 +1,9 @@
 // pages/generate/chart/chart.js
 import * as echarts from '../../../components/ec-canvas/echarts';
 
+var heightGlobal, widthGLobal, canvasGlobal, dprGlobal, chartNow;
 var chart_template = 0;
+var chartType = [];
 
 // 1号chart的数据
 const data = [["2000-06-05", 116], ["2000-06-06", 129], ["2000-06-07", 135], ["2000-06-08", 86], ["2000-06-09", 73], ["2000-06-10", 85], ["2000-06-11", 73], ["2000-06-12", 68], ["2000-06-13", 92], ["2000-06-14", 130], ["2000-06-15", 245], ["2000-06-16", 139], ["2000-06-17", 115], ["2000-06-18", 111], ["2000-06-19", 309], ["2000-06-20", 206], ["2000-06-21", 137], ["2000-06-22", 128], ["2000-06-23", 85], ["2000-06-24", 94], ["2000-06-25", 71], ["2000-06-26", 106], ["2000-06-27", 84], ["2000-06-28", 93], ["2000-06-29", 85], ["2000-06-30", 73], ["2000-07-01", 83], ["2000-07-02", 125], ["2000-07-03", 107], ["2000-07-04", 82], ["2000-07-05", 44], ["2000-07-06", 72], ["2000-07-07", 106], ["2000-07-08", 107], ["2000-07-09", 66], ["2000-07-10", 91], ["2000-07-11", 92], ["2000-07-12", 113], ["2000-07-13", 107], ["2000-07-14", 131], ["2000-07-15", 111], ["2000-07-16", 64], ["2000-07-17", 69], ["2000-07-18", 88], ["2000-07-19", 77], ["2000-07-20", 83], ["2000-07-21", 111], ["2000-07-22", 57], ["2000-07-23", 55], ["2000-07-24", 60]];
@@ -147,13 +149,18 @@ const itemStyle = {
 
 
 function initChart(canvas, width, height, dpr) {
-  const chart = echarts.init(canvas, null, {
-    width: width,
-    height: height,
-    devicePixelRatio: dpr
+  heightGlobal = height;
+  widthGLobal = width;
+  canvasGlobal = canvas;
+  dprGlobal = dpr;
+  const chart = echarts.init(canvasGlobal, null, {
+    width: widthGLobal,
+    height: heightGlobal,
+    devicePixelRatio: dprGlobal
   });
-  canvas.setChart(chart);
-  const chartType = [
+  canvasGlobal.setChart(chart);
+
+  chartType = [
     // 0号chart
     {
       color: ['#80FFA5', '#00DDFF', '#37A2FF', '#FF0087', '#FFBF00'],
@@ -601,6 +608,7 @@ function initChart(canvas, width, height, dpr) {
     }
   ];
   chart.setOption(chartType[chart_template]);
+  chartNow = chart;
   return chart;
 }
 
@@ -611,6 +619,12 @@ Page({
    * 页面的初始数据
    */
   data: {
+    templates:[
+      {id: 0, selected: false},
+      {id: 1, selected: false},
+      {id: 2, selected: false},
+      {id: 3, selected: false}
+    ],
     keys:[
       {info: '1111', selected: false},
       {info: '2222', selected: false},
@@ -645,6 +659,28 @@ Page({
       ['keys[' + index + '].selected']: this.data.keys[index].selected
     });
   },
+  changeTemplate: function(e){
+    const { index } = e.currentTarget.dataset;
+    if(index == chart_template)
+      return;
+    this.setData({
+      ['templates[' + chart_template + '].selected']: false
+    });
+    chart_template = index;
+    this.setData({
+      ['templates[' + index + '].selected']: true
+    });
+    chartNow.clear();
+    const chart = echarts.init(canvasGlobal, null, {
+      width: widthGLobal,
+      height: heightGlobal,
+      devicePixelRatio: dprGlobal
+    });
+    canvasGlobal.setChart(chart);
+    const option = chartType[chart_template];
+    chartNow.setOption(option);
+    chartNow = chart;
+  },
   handleSave() {
     const ecComponent = this.selectComponent('#echart');
     // 先保存图片到临时的本地文件，然后存入系统相册
@@ -677,6 +713,9 @@ Page({
    */
   onLoad(options) {
     chart_template = options.index;
+    this.setData({
+      ['templates[' + chart_template + '].selected']: true
+    });
   },
 
   /**
