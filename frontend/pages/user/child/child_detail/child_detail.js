c10434e9fe64fd4f68724c246f192d987c78acdf
leishaforlinminzhi <15611664060@qq.com>
2023-11-28 20:28:39 +0800

    feat(front): add 2 charts in child detail page

--- frontend/pages/user/child/child_detail/child_detail.js
+++ frontend/pages/user/child/child_detail/child_detail.js
@@ -4,12 +4,47 @@ const app = getApp();
 
 var child_id = 0;
 
+var plans = 0;
+var icon_list = [];
+var events = 0;
+var event_list = [];
+var event_tag_list =  [];
+var texts = 0;
+var text_tag_list = [];
+
 var colorSet = [
   {id: 0, backgroundColor: '#143e64', colors:["#87ceeb","#59c4e6","#a5e7f0", '#add8e6', '#b5e4e6','#7cb9e8', '#5c9cc2', '#87ceeb', '#add8e6', '#164a7a98']},
   {id: 1, backgroundColor: '#b27466', colors:['#ffd700', '#f0e68c', '#eedc82', '#ffec8b','#ffd700', '#ffdb58', '#f0e68c', '#eedc82', '#ffec8b', '#b9612791']},
   {id: 2, backgroundColor: '#00207496', colors:['#c23531', '#ff6f94', '#ff5983', '#f4506e','#f48fb1', '#ff9eb4', '#ff6f94', '#ff5983', '#f4506e', '#23007471']},
   {id: 3, backgroundColor: '#008080', colors:['#a4ebb1', '#bdf9ca', '#c7fdc9', '#d3ffc8', '#e1ffdb', '#e7ffe5', '#c4f5c7', '#a4e7a0', '#83d968','#a4ebb1', ]},
 ];
+
+var dataFortag1 = [];
+var mindataFortag1 = 100;
+var maxdataFortag1 = 0;
+
+function initData() {
+  dataFortag1 = event_tag_list.map(item => {
+    return {
+      value: item.number,
+      name: item.tag
+    };
+  });
+
+  for (let i = 0; i < dataFortag1.length; i++) {
+    if (dataFortag1[i].value > maxdataFortag1) {
+      maxdataFortag1 = dataFortag1[i].value;
+    }
+  
+    if (dataFortag1[i].value < mindataFortag1) {
+      mindataFortag1 = dataFortag1[i].value;
+    }
+  }
+  maxdataFortag1 = maxdataFortag1 * 1.5 + 2;
+  mindataFortag1 = mindataFortag1 * 0.8 - 2; 
+  console.log('min:',mindataFortag1)
+  console.log('max:',maxdataFortag1)
+}
 
 function initRadarChart(canvas, width, height, dpr) {
   const chart = echarts.init(canvas, null, {
@@ -109,8 +144,8 @@ function initTag1Chart(canvas, width, height, dpr) {
     },
     visualMap: {
       show: false,
-      min: 80,
-      max: 600,
+      min: mindataFortag1,
+      max: maxdataFortag1,
       inRange: {
         colorLightness: [0, 1]
       }
@@ -121,13 +156,7 @@ function initTag1Chart(canvas, width, height, dpr) {
         type: 'pie',
         radius: '50%',
         center: ['32%', '30%'],
-        data: [
-          { value: 335, name: '运动' },
-          { value: 310, name: '学习' },
-          { value: 274, name: '艺术' },
-          { value: 235, name: '阅读' },
-          { value: 400, name: '自然' }
-        ].sort(function (a, b) {
+        data: dataFortag1.sort(function (a, b) {
           return a.value - b.value;
         }),
         roseType: 'radius',
@@ -143,6 +172,8 @@ function initTag1Chart(canvas, width, height, dpr) {
         },
         itemStyle: {
           color: colorSet[child_id].colors[0],
+          shadowBlur: 10,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
         },
         animationType: 'scale',
         animationEasing: 'elasticOut',
@@ -152,6 +183,110 @@ function initTag1Chart(canvas, width, height, dpr) {
       }
     ]
   };
+  chart.setOption(option);
+  return chart;
+}
+
+function initEventChart(canvas, width, height, dpr) {
+  const chart = echarts.init(canvas, null, {
+    width: width,
+    height: height,
+    devicePixelRatio: dpr
+  });
+  canvas.setChart(chart);
+  const option = {
+    color:colorSet[child_id].colors,
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'cross',
+        label: {
+          backgroundColor: '#6a7985'
+        }
+      }
+    },
+    legend: {
+      data: ['事件总数', '学习', '阅读', '运动', '艺术']
+    },
+    grid: {
+      left: '3%',
+      right: '4%',
+      bottom: '3%',
+      containLabel: true
+    },
+    xAxis: [
+      {
+        type: 'category',
+        boundaryGap: false,
+        data: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+        axisTick:{
+          length: 1,
+        }
+      }
+    ],
+    yAxis: [
+      {
+        axisTick: { show: false },
+        type: 'value'
+      }
+    ],
+    series: [
+      {
+        name: '事件总数',
+        type: 'bar',
+        stack: 'Total',
+        areaStyle: {},
+        emphasis: {
+          focus: 'series'
+        },
+        data: event_list
+      },
+      // {
+      //   name: '学习',
+      //   type: 'line',
+      //   stack: 'Total',
+      //   areaStyle: {},
+      //   emphasis: {
+      //     focus: 'series'
+      //   },
+      //   data: [22, 18, 19, 23, 29, 33, 31]
+      // },
+      // {
+      //   name: '运动',
+      //   type: 'line',
+      //   stack: 'Total',
+      //   areaStyle: {},
+      //   emphasis: {
+      //     focus: 'series'
+      //   },
+      //   data: [15, 23, 20, 15, 19, 33, 41]
+      // },
+      // {
+      //   name: '阅读',
+      //   type: 'line',
+      //   stack: 'Total',
+      //   areaStyle: {},
+      //   emphasis: {
+      //     focus: 'series'
+      //   },
+      //   data: [32, 33, 30, 33, 39, 33, 32]
+      // },
+      // {
+      //   name: '艺术',
+      //   type: 'line',
+      //   stack: 'Total',
+      //   label: {
+      //     show: true,
+      //     position: 'top'
+      //   },
+      //   areaStyle: {},
+      //   emphasis: {
+      //     focus: 'series'
+      //   },
+      //   data: [8, 9, 9, 9, 12, 13, 13]
+      // }
+    ]
+  }
   chart.setOption(option);
   return chart;
 }
@@ -176,11 +311,6 @@ function initTag2Chart(canvas, width, height, dpr) {
     },
     legend: {
       data: ['自然', '学习', '阅读', '运动', '艺术']
-    },
-    toolbox: {
-      feature: {
-        saveAsImage: {}
-      }
     },
     grid: {
       left: '3%',
@@ -276,6 +406,9 @@ Page({
     ec_tag1: {
       onInit: initTag1Chart
     },
+    ec_event: {
+      onInit: initEventChart
+    },
     ec_tag2: {
       onInit: initTag2Chart
     },
@@ -295,7 +428,6 @@ Page({
    */
   onLoad(options) {
     child_id = options.index;
-    console.log(options.name)
     var that = this
     this.setData({
       name: options.name
@@ -304,7 +436,13 @@ Page({
       url: this.data.host_+'user/api/user/child_detail'+'?name='+this.data.name,
       method: 'GET',
       success: function(res) {
-        console.log(res)
+        plans =  res.data.child_item.plans,
+        icon_list = res.data.child_item.icon_list,
+        events = res.data.child_item.events,
+        event_list = res.data.child_item.event_list,
+        event_tag_list = res.data.child_item.event_tag_list,
+        texts = res.data.child_item.texts,
+        text_tag_list = res.data.child_item.text_tag_list 
         that.setData({
           plans: res.data.child_item.plans,
           icon_list: res.data.child_item.icon_list,
@@ -321,7 +459,7 @@ Page({
         console.log('event_tag_list:', that.data.event_tag_list)
         console.log('texts:', that.data.texts)
         console.log('text_tag_list:', that.data.text_tag_list)
-
+        initData();
       }
     })
   },
