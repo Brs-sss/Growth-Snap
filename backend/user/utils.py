f582b0f5f1b8a75c5157f097176bafb0903e62a1
Brs-sss <brs21@mails.tsinghua.edu.cn>
2023-11-30 11:04:33 +0800

    other

--- backend/user/utils.py
+++ backend/user/utils.py
@@ -18,8 +18,8 @@ from PIL import Image
 import requests
 
 # 指定 wkhtmltopdf 可执行文件路径
-#config = pdfkit.configuration(wkhtmltopdf='D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
-config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
+config = pdfkit.configuration(wkhtmltopdf='D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
+# config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')
 
 render_path = 'static/template/rendered/'
 
