e1f5fdbf2e96e18cb28184bf16d9ebea7afaf355
Runsheng Bai <105222750+Brs-sss@users.noreply.github.com>
2024-01-02 22:03:36 +0800

    fix(back): restore the path for wkhtmltopdf

--- backend/user/utils.py
+++ backend/user/utils.py
@@ -27,8 +27,8 @@ import requests
 
 
 # 指定 wkhtmltopdf 可执行文件路径
-config = pdfkit.configuration(wkhtmltopdf='D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')  #windows
-# config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')  # linux
+# config = pdfkit.configuration(wkhtmltopdf='D:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')  #windows
+config = pdfkit.configuration(wkhtmltopdf='/usr/bin/wkhtmltopdf')  # linux
 # config = pdfkit.configuration(wkhtmltopdf='/usr/local/bin/wkhtmltopdf')  #mac
  
 render_path = 'static/template/rendered/'
